fb85c37558baa2d2da468f2936fd7373b5b0f0d5
August <augustinekoketso4@gmail.com>
2025-11-21 17:30:06 +0200

    added the debug screen files

---
+++ debugScreen.c
@@ -0,0 +1,20 @@
+
+#include <ncurses.h>
+#include "debugScreen.h"
+
+
+void
+setScreen(machine *m)
+{
+    return;
+
+}
+
+void
+
+updateScreen(machine *m)
+{
+    return;
+
+}
+
---
+++ debugScreen.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "machine.h"
+
+
+void setScreen(machine *m);
+void updateScreen(machine *m);
+
