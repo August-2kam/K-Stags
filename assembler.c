293101a0a79186eccea914a1de0932a89a0b3990
August-2kam <august2kam@gmail.com>
2025-11-30 16:44:35 +0200

    read mnemonics

--- assembler.c
+++ assembler.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 
 
+#define currChar(b) b->buffer[bufPointer]
+#define advanceBufPointer(b) b->bufPointer++
+#define endOfBuffer(b) b->bufPointer >= b->bufferSize
+
 typedef struct 
 {
     char    *buffer;
+    size_t  bufferSize;
     size_t  bufPointer;
+    int col,line;
+}srcBuffer;
+
+
+
+//skip white spaces in the 
+void 
+skipSpaces(srcBuffer *sb)
+{
+    while(!(endOfBuffer(sb)) && isspace(currChar(sb)))
+    {
+        if(currChar(sb) == '\n') sb->line++;
+        advanceBufPointer(sb);
+
+    }
+}
+
+int
+readMnemonic(srcBuffer *sb, char **start)
+{
+    skipSpaces(sb);
+    *start = sb->buffer + sb->bufPointer;
+    int size = 0;
+
+    char c = currChar(sb);
+    while(c != '\n' ||
+          c != ' '  ||
+          c != '\0') size++;
+
+
+    return size ;    
 }
 
 
